468dba1444a600ea0d38fc3feef2141738a5ee89
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-06-09 13:57:24 +0200

    Task 3. Player profile

--- js/task-3.js
+++ js/task-3.js
@@ -8,4 +8,26 @@ function filterArray(numbers, value) {
   return filteredArray;
 }
 
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime = hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
 console.log(" Task 3. Player profile ");
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
